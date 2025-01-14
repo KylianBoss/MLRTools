71e93601659401f9663b2b83aa63398fd75c1663
Kylian Boss <kylian.boss@bossconcepts.ch>
2025-01-14 08:18:25 +0100

    Add a new function to clear the dayResumes table

--- src/pages/admin/DatabaseSettings.vue
+++ src/pages/admin/DatabaseSettings.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding>
     <div class="text-h4">Database</div>
-    <div class="row">
+    <div class="row q-py-xs">
       <div class="col">
         <q-btn
           label="Synchroniser les modèles"
@@ -12,13 +12,53 @@
         />
       </div>
     </div>
+    <div class="row q-py-xs">
+      <div class="col">
+        <q-btn
+          label="Vider la table day resume"
+          color="primary"
+          @click="DB.emptyDayResume"
+          class="full-width"
+          :loading="DB.loadingState"
+        />
+      </div>
+    </div>
+    <div class="row q-py-xs">
+      <div class="col">
+        <q-btn
+          label="Vider la table day resume à date précise"
+          color="primary"
+          @click="selectDateToEmpty"
+          class="full-width"
+          :loading="DB.loadingState"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import { useDBStore } from "stores/db";
+import { useQuasar } from "quasar";
 
 const DB = useDBStore();
+const $q = useQuasar();
+
+const selectDateToEmpty = () => {
+  $q.dialog({
+    title: "Vider la table day resume à date précise",
+    message: "Saisir la date à laquelle vider la table",
+    prompt: {
+      label: "Date",
+      type: "date",
+      mask: "##.##.####",
+      model: null,
+    },
+    persistent: true,
+  }).onOk((data) => {
+    DB.emptyDayResumeAtDate(data);
+  });
+};
 </script>
 
 <style></style>
