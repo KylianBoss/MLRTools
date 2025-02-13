6431821875bfd7ff74e6d8bc1262ab8740986fed
Kylian Boss <kylian.boss@bossconcepts.ch>
2025-02-13 08:38:39 +0100

    Add HomePage component and update router to use it as the home route

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h1 class="text-h6">Home</h1>
+      <p class="text-subtitle1">This is the Home page.</p>
+    </div>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,12 @@ const routes = [
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", name: "home", component: () => import("pages/KPI.vue") },
+      {
+        path: "",
+        name: "home",
+        component: () => import("pages/HomePage.vue"),
+      },
+      { path: "kpi", name: "kpi", component: () => import("pages/KPI.vue") },
       {
         path: "search",
         name: "search",
