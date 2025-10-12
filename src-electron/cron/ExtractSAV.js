4e3f2c28549a5a720a0be868877a3a17d680cff7
Kylian Boss <kylian.boss@bossconcepts.ch>
2025-10-12 22:58:38 +0200

    feat: update version to 1.7.23 in package.json; enhance ExtractSAV job with error handling and logging improvements RELEASE

--- src-electron/cron/ExtractSAV.js
+++ src-electron/cron/ExtractSAV.js
@@ -26,145 +26,158 @@ export const extractSAV = async () => {
     jobName
   );
 
-  const dateToGet = dayjs().format("YYYY-MM-DD");
-  const fileName = `SAV-Export-AlarmLog_${dayjs(dateToGet).format(
-    "YYYYMMDD"
-  )}_000000.csv`;
-  const filePath = path.join(SAV_EXPORT_PATH, fileName);
+  try {
+    const dateToGet = dayjs().format("YYYY-MM-DD");
+    const fileName = `SAV-Export-AlarmLog_${dayjs(dateToGet).format(
+      "YYYYMMDD"
+    )}_000000.csv`;
+    const filePath = path.join(SAV_EXPORT_PATH, fileName);
 
-  if (!fs.existsSync(filePath)) {
-    console.warn(`File ${fileName} does not exist, skipping...`);
+    if (!fs.existsSync(filePath)) {
+      console.warn(`File ${fileName} does not exist, skipping...`);
+      await updateJob(
+        {
+          lastRun: new Date(),
+          lastLog: `File ${fileName} does not exist, skipping...`,
+          endAt: new Date(),
+          actualState: "error",
+        },
+        jobName
+      );
+      return;
+    }
+
     await updateJob(
       {
         lastRun: new Date(),
-        lastLog: `File ${fileName} does not exist, skipping...`,
+        lastLog: `Downloading file: ${fileName}`,
+      },
+      jobName
+    );
+    const data = [];
+    await new Promise((resolve) => {
+      fs.createReadStream(filePath, "utf8")
+        .pipe(csv({ separator: ";" }))
+        .on("data", (row) => data.push(row))
+        .on("end", async () => {
+          console.log(`Downloaded file: ${fileName}`);
+          await updateJob(
+            {
+              lastRun: new Date(),
+              lastLog: `Downloaded file: ${fileName}`,
+            },
+            jobName
+          );
+          resolve();
+        });
+    });
+
+    const formattedData = data.map((line) => {
+      const rawData = line.replaceAll('"', "").split(";");
+      if (rawData.length < 11) return null;
+      if (!!!rawData[0].match(/\d/)) return null;
+      const startDate = dayjs(rawData[1], "D MMM YYYY à HH:mm:ss", "fr");
+      const endDate = dayjs(rawData[2], "D MMM YYYY à HH:mm:ss", "fr");
+      if (!startDate.isValid() || !endDate.isValid()) return null;
+      if (rawData[7] === "M6009.0306") return null; // Don't put in DB the warning from the shuttle
+      if (rawData[7] === "M6130.0201") return null; // Don't put in DB the warning from the shuttle
+      if (rawData[7] === "M6130.0203") return null; // Don't put in DB the warning from the shuttle
+      if (rawData[7] === "M6130.0202") return null; // Don't put in DB the warning from the shuttle
+      return {
+        dbId: rawData[0],
+        timeOfOccurence: startDate.format("YYYY-MM-DD HH:mm:ss"),
+        timeOfAcknowledge: endDate.format("YYYY-MM-DD HH:mm:ss"),
+        duration: dayjs.duration(endDate.diff(startDate)).asSeconds(),
+        dataSource: rawData[5],
+        alarmArea: rawData[6],
+        alarmCode: rawData[7],
+        alarmText: rawData[8],
+        severity: rawData[9],
+        classification: rawData[10],
+        assignedUser: rawData[12],
+        alarmId: `${rawData[5]}.${rawData[6]}.${rawData[7]}`.toLowerCase(),
+      };
+    });
+
+    const alarms = await db.models.Datalog.bulkCreate(formattedData, {
+      updateOnDuplicate: [
+        "timeOfOccurence",
+        "timeOfAcknowledge",
+        "duration",
+        "dataSource",
+        "alarmArea",
+        "alarmCode",
+        "alarmText",
+        "severity",
+        "classification",
+        "assignedUser",
+        "alarmId",
+      ],
+    });
+    console.log(
+      `Data for ${dateToGet} ${alarms.length} inserted/updated into database.`
+    );
+    await updateJob(
+      {
+        lastRun: new Date(),
+        lastLog: `Data for ${dateToGet} ${alarms.length} inserted/updated into database.`,
+      },
+      jobName
+    );
+
+    // Keep only unique alarms from the inserted alarms
+    const uniqueAlarms = alarms.filter(
+      (alarm, index, self) =>
+        index === self.findIndex((a) => a.alarmId === alarm.alarmId)
+    );
+
+    for (const alarm of uniqueAlarms) {
+      await db.models.Alarms.upsert({
+        alarmId: alarm.alarmId,
+        dataSource: alarm.dataSource,
+        alarmArea: alarm.alarmArea,
+        alarmCode: alarm.alarmCode,
+        alarmText: alarm.alarmText,
+      });
+    }
+    console.log(
+      `Alarms table updated with ${uniqueAlarms.length} unique alarms.`
+    );
+    await updateJob(
+      {
+        lastRun: new Date(),
+        lastLog: `Alarms table updated with ${uniqueAlarms.length} unique alarms.`,
+      },
+      jobName
+    );
+
+    console.log("SAV extraction completed.");
+    await updateJob(
+      {
+        lastRun: new Date(),
+        lastLog: "SAV extraction completed.",
+        endAt: new Date(),
+        actualState: "idle",
+      },
+      jobName
+    );
+    // Set the bot to restart
+    const bot = await db.models.Users.findOne({
+      where: { isBot: true },
+    });
+    if (bot) {
+      bot.update({ needsRestart: true });
+    }
+  } catch (error) {
+    console.error("Error during SAV extraction:", error);
+    await updateJob(
+      {
+        lastRun: new Date(),
+        lastLog: `Error during SAV extraction: ${error.message}`,
         endAt: new Date(),
         actualState: "error",
       },
       jobName
     );
-    return;
-  }
-
-  await updateJob(
-    {
-      lastRun: new Date(),
-      lastLog: `Downloading file: ${fileName}`,
-    },
-    jobName
-  );
-  const data = [];
-  await new Promise((resolve) => {
-    fs.createReadStream(filePath, "utf8")
-      .pipe(csv({ separator: ";" }))
-      .on("data", (row) => data.push(row))
-      .on("end", async () => {
-        console.log(`Downloaded file: ${fileName}`);
-        await updateJob(
-          {
-            lastRun: new Date(),
-            lastLog: `Downloaded file: ${fileName}`,
-          },
-          jobName
-        );
-        resolve();
-      });
-  });
-
-  const formattedData = data.map((line) => {
-    const rawData = line.replaceAll('"', "").split(";");
-    if (rawData.length < 11) return null;
-    if (!!!rawData[0].match(/\d/)) return null;
-    const startDate = dayjs(rawData[1], "D MMM YYYY à HH:mm:ss", "fr");
-    const endDate = dayjs(rawData[2], "D MMM YYYY à HH:mm:ss", "fr");
-    if (!startDate.isValid() || !endDate.isValid()) return null;
-    if (rawData[7] === "M6009.0306") return null; // Don't put in DB the warning from the shuttle
-    if (rawData[7] === "M6130.0201") return null; // Don't put in DB the warning from the shuttle
-    if (rawData[7] === "M6130.0203") return null; // Don't put in DB the warning from the shuttle
-    if (rawData[7] === "M6130.0202") return null; // Don't put in DB the warning from the shuttle
-    return {
-      dbId: rawData[0],
-      timeOfOccurence: startDate.format("YYYY-MM-DD HH:mm:ss"),
-      timeOfAcknowledge: endDate.format("YYYY-MM-DD HH:mm:ss"),
-      duration: dayjs.duration(endDate.diff(startDate)).asSeconds(),
-      dataSource: rawData[5],
-      alarmArea: rawData[6],
-      alarmCode: rawData[7],
-      alarmText: rawData[8],
-      severity: rawData[9],
-      classification: rawData[10],
-      assignedUser: rawData[12],
-      alarmId: `${rawData[5]}.${rawData[6]}.${rawData[7]}`.toLowerCase(),
-    };
-  });
-
-  const alarms = await db.models.Datalog.bulkCreate(formattedData, {
-    updateOnDuplicate: [
-      "timeOfOccurence",
-      "timeOfAcknowledge",
-      "duration",
-      "dataSource",
-      "alarmArea",
-      "alarmCode",
-      "alarmText",
-      "severity",
-      "classification",
-      "assignedUser",
-      "alarmId",
-    ],
-  });
-  console.log(
-    `Data for ${dateToGet} ${alarms.length} inserted/updated into database.`
-  );
-  await updateJob(
-    {
-      lastRun: new Date(),
-      lastLog: `Data for ${dateToGet} ${alarms.length} inserted/updated into database.`,
-    },
-    jobName
-  );
-
-  // Keep only unique alarms from the inserted alarms
-  const uniqueAlarms = alarms.filter(
-    (alarm, index, self) =>
-      index === self.findIndex((a) => a.alarmId === alarm.alarmId)
-  );
-
-  for (const alarm of uniqueAlarms) {
-    await db.models.Alarms.upsert({
-      alarmId: alarm.alarmId,
-      dataSource: alarm.dataSource,
-      alarmArea: alarm.alarmArea,
-      alarmCode: alarm.alarmCode,
-      alarmText: alarm.alarmText,
-    });
-  }
-  console.log(
-    `Alarms table updated with ${uniqueAlarms.length} unique alarms.`
-  );
-  await updateJob(
-    {
-      lastRun: new Date(),
-      lastLog: `Alarms table updated with ${uniqueAlarms.length} unique alarms.`,
-    },
-    jobName
-  );
-
-  console.log("SAV extraction completed.");
-  await updateJob(
-    {
-      lastRun: new Date(),
-      lastLog: "SAV extraction completed.",
-      endAt: new Date(),
-      actualState: "idle",
-    },
-    jobName
-  );
-  // Set the bot to restart
-  const bot = await db.models.Users.findOne({
-    where: { isBot: true },
-  });
-  if (bot) {
-    bot.update({ needsRestart: true });
   }
 };
