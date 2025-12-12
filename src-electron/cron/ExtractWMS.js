bc579ce04a9c03a543405d6ef0a1024cfc931d68
Kylian Boss <63243626+KylianBoss@users.noreply.github.com>
2025-12-12 08:50:51 +0100

    Change loop limit from 23 to 24 in ExtractWMS.js

--- src-electron/cron/ExtractWMS.js
+++ src-electron/cron/ExtractWMS.js
@@ -58,7 +58,7 @@ export const extractWMS = async () => {
     for (const date of datesToProcess) {
       const data = [];
       let getFile = false;
-      for (let i = 0; i < 23; i++) {
+      for (let i = 0; i < 24; i++) {
         const fileName = `SUIVI_${dayjs(date).format("YYYYMMDD")}_${i
           .toString()
           .padStart(2, "0")}.csv`;
