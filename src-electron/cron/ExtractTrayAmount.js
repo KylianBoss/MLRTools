821f46ca4c5da43dc9f316b4e3697298d5cf89ea
Kylian Boss <kylian.boss@bossconcepts.ch>
2025-12-12 16:38:00 +0100

    Bump version to 1.7.60 and enhance data saving and validation in extractTrayAmount function RELEASE

--- src-electron/cron/ExtractTrayAmount.js
+++ src-electron/cron/ExtractTrayAmount.js
@@ -386,325 +386,17 @@ export const extractTrayAmount = (date, headless = true) => {
         jobName
       );
 
-      // for (const zone of zones) {
-      //   for (const readPoint of zone.readPoints) {
-      //     let i = 1;
-      //     for (const split of splits) {
-      //       await updateJob(
-      //         {
-      //           actualState: "running",
-      //           lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //           lastLog: `Processing : ${zone.zone} - ${readPoint} - Split: ${i}`,
-      //         },
-      //         jobName
-      //       );
-      //       const startDateInput = await page.$(
-      //         'input[id="inputTimestampPicker_1"]'
-      //       );
-      //       const endDateInput = await page.$(
-      //         'input[id="inputTimestampPicker_2"]'
-      //       );
-      //       const addressInput = await page.$('input[id="input_2"]');
-      //       const messageTypeInput = await page.$('input[id="input_5"]');
-      //       if (
-      //         !startDateInput ||
-      //         !endDateInput ||
-      //         !addressInput ||
-      //         !messageTypeInput
-      //       ) {
-      //         await updateJob(
-      //           {
-      //             actualState: "running",
-      //             lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //             lastLog: `An error has occured, one of the input field was not found on the page`,
-      //             endAt: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //             actualState: "error",
-      //           },
-      //           jobName
-      //         );
-      //         throw new Error("One or more input fields not found on the page");
-      //       }
+      sleep(2000);
 
-      //       await clearInput(startDateInput, page);
-      //       await startDateInput.type(
-      //         dayjs(split.start).format("DD.MM.YYYY HH:mm:ss")
-      //       );
-
-      //       await clearInput(endDateInput, page);
-      //       await endDateInput.type(
-      //         dayjs(split.end).format("DD.MM.YYYY HH:mm:ss")
-      //       );
-
-      //       await clearInput(addressInput, page);
-      //       await addressInput.type(readPoint);
-
-      //       await clearInput(messageTypeInput, page);
-      //       await messageTypeInput.type(zone.messageType);
-
-      //       console.log("Filling form...");
-      //       await updateJob(
-      //         {
-      //           actualState: "running",
-      //           lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //           lastLog: `Sending form for : ${zone.zone} - ${readPoint} - Split: ${i}`,
-      //         },
-      //         jobName
-      //       );
-      //       await Promise.all([
-      //         page.waitForNavigation({
-      //           waitUntil: "networkidle0",
-      //           timeout: 120000,
-      //         }),
-      //         page.click('input[name="Search"]'),
-      //       ]);
-
-      //       await updateJob(
-      //         {
-      //           actualState: "running",
-      //           lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //           lastLog: `Form loaded for : ${zone.zone} - ${readPoint} - Split: ${i}`,
-      //         },
-      //         jobName
-      //       );
-
-      //       await page.waitForSelector('a[href*=".csv"]', {
-      //         timeout: 120000,
-      //       });
-
-      //       const pageContent = await page.content();
-      //       if (pageContent.includes("Aucun jeu de données")) {
-      //         await updateJob(
-      //           {
-      //             actualState: "running",
-      //             lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //             lastLog: `Nothing to import from : ${zone.zone} - ${readPoint} - Split: ${i}`,
-      //           },
-      //           jobName
-      //         );
-      //         i++;
-      //         continue;
-      //       }
-
-      //       function download() {
-      //         return new Promise(async (resolve, reject) => {
-      //           const downloadLink = await page.$('a[href*=".csv"]');
-      //           if (downloadLink) {
-      //             await downloadLink.click();
-      //             console.log("Start downloading CSV file...");
-      //             await updateJob(
-      //               {
-      //                 actualState: "running",
-      //                 lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //                 lastLog: `Start downloading CSV file for address ${readPoint} in group ${zone.zone} for split ${i}`,
-      //               },
-      //               jobName
-      //             );
-      //           } else {
-      //             console.warn(
-      //               `No download link found for address ${readPoint} in group ${zone.zone} for split ${i}`
-      //             );
-      //             resolve();
-      //           }
-
-      //           let safe = 0;
-      //           do {
-      //             console.log("Downloading...");
-      //             await sleep(3000);
-      //             safe++;
-      //             if (safe > 20) {
-      //               console.warn(
-      //                 `Download took too long for address ${readPoint} in group ${zone.zone} for split ${i}`
-      //               );
-      //               return reject();
-      //             }
-      //           } while (
-      //             !fs.existsSync(
-      //               path.join(
-      //                 process.cwd(),
-      //                 "storage",
-      //                 "downloads",
-      //                 "PLC Interface.csv"
-      //               )
-      //             )
-      //           );
-      //           console.log("CSV file downloaded successfully");
-      //           await updateJob(
-      //             {
-      //               actualState: "running",
-      //               lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //               lastLog: `CSV file downloaded successfully for address ${readPoint} in group ${zone.zone} for split ${i}`,
-      //             },
-      //             jobName
-      //           );
-
-      //           fs.renameSync(
-      //             path.join(
-      //               process.cwd(),
-      //               "storage",
-      //               "downloads",
-      //               "PLC Interface.csv"
-      //             ),
-      //             path.join(
-      //               process.cwd(),
-      //               "storage",
-      //               "downloads",
-      //               `${readPoint}_${i}.csv`
-      //             )
-      //           );
-      //           return resolve();
-      //         });
-      //       }
-
-      //       let downloaded = false;
-      //       while (!downloaded) {
-      //         await download()
-      //           .then(() => (downloaded = true))
-      //           .catch(() => (downloaded = false));
-      //       }
-      //       i++;
-      //     }
-      //   }
-      // }
-
-      // console.log(
-      //   "All readPoints downloaded successfully, starting extraction..."
-      // );
-      // global.sendNotificationToElectron(
-      //   "Extract tray amount",
-      //   "All readPoints downloaded successfully, starting extraction..."
-      // );
-      // await updateJob(
-      //   {
-      //     actualState: "running",
-      //     lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //     lastLog: `All readPoints downloaded successfully, starting extraction...`,
-      //   },
-      //   jobName
-      // );
-      // await browser.close();
-
-      // for (const zone of zones) {
-      //   console.log(`Extracting data for group: ${zone.zone}`);
-      //   let zoneData = [];
-      //   for (const readPoint of zone.readPoints) {
-      //     console.log(`Processing address: ${readPoint}`);
-      //     let i = 1;
-      //     let splitGroup = [];
-      //     for (const split_ of splits) {
-      //       console.log(
-      //         `Processing split ${i} for address ${readPoint} in group ${zone.zone}`
-      //       );
-      //       await updateJob(
-      //         {
-      //           actualState: "running",
-      //           lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //           lastLog: `Processing split ${i} for address ${readPoint} in group ${zone.zone}`,
-      //         },
-      //         jobName
-      //       );
-      //       let results = [];
-      //       if (
-      //         !fs.existsSync(
-      //           path.join(
-      //             process.cwd(),
-      //             "storage",
-      //             "downloads",
-      //             `${readPoint}_${i}.csv`
-      //           )
-      //         )
-      //       ) {
-      //         i++;
-      //         console.warn(
-      //           `File not found for address ${readPoint} in group ${zone.zone} for split ${i}`
-      //         );
-      //         continue;
-      //       }
-      //       await new Promise((resolve, reject) => {
-      //         fs.createReadStream(
-      //           path.join(
-      //             process.cwd(),
-      //             "storage",
-      //             "downloads",
-      //             `${readPoint}_${i}.csv`
-      //           ),
-      //           "utf8"
-      //         )
-      //           .pipe(csv({ separator: ";" }))
-      //           .on("data", (data) => results.push(data))
-      //           .on("end", async () => {
-      //             results = results.map((d) => {
-      //               const dateRegex =
-      //                 /(\d{2}\.\d{2}\.\d{4}) (\d{2}:\d{2}:\d{2}),/;
-      //               const match = d.EVENT_TIME.match(dateRegex);
-      //               let dateTime = "";
-      //               if (!match) {
-      //                 console.warn(
-      //                   `Invalid date format for address ${readPoint} in group ${zone.zone} for split ${i}`
-      //                 );
-      //                 dateTime = dayjs(split_.start).format(
-      //                   "YYYY-MM-DD HH:mm:ss"
-      //                 );
-      //               } else {
-      //                 dateTime = dayjs(
-      //                   `${match[1]} ${match[2]}`,
-      //                   "DD.MM.YYYY HH:mm:ss"
-      //                 ).format("YYYY-MM-DD HH:mm:ss");
-      //               }
-      //               return {
-      //                 timestamp: dateTime,
-      //                 trayId: d.loadCarrierIdentifier,
-      //                 address: d.addressidentifier,
-      //               };
-      //             });
-      //             console.log(
-      //               `Extracted ${results.length} records for address ${readPoint} in group ${zone.zone} for split ${i}`
-      //             );
-      //             await updateJob(
-      //               {
-      //                 actualState: "running",
-      //                 lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //                 lastLog: `Extracted ${results.length} records for address ${readPoint} in group ${zone.zone} for split ${i}`,
-      //               },
-      //               jobName
-      //             );
-      //             splitGroup = [...splitGroup, ...results];
-      //             resolve();
-      //           });
-      //       });
-      //       i++;
-      //     }
-      //     zoneData = [...zoneData, ...splitGroup];
-      //   }
-      //   console.log(
-      //     `Total records extracted for group ${zone.zone}: ${zoneData.length}`
-      //   );
-      //   fs.writeFileSync(
-      //     path.join(
-      //       process.cwd(),
-      //       "storage",
-      //       "archives",
-      //       `${dayjs(date).format("YYYY_MM_DD")}_${zone.zone}.json`
-      //     ),
-      //     JSON.stringify(zoneData, null, 2)
-      //   );
-      //   zone.total = zoneData.length;
-      // }
-      // console.log("Extraction completed successfully");
-      // global.sendNotificationToElectron(
-      //   "Extract tray amount",
-      //   "Extraction completed successfully"
-      // );
-      // await updateJob(
-      //   {
-      //     actualState: "idle",
-      //     lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //     lastLog: `Extraction completed successfully`,
-      //     endAt: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      //     args: null,
-      //     cronExpression: "0 1 * * * ",
-      //   },
-      //   jobName
-      // );
+      console.log("Saving data...");
+      await updateJob(
+        {
+          actualState: "running",
+          lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+          lastLog: `Saving data...`,
+        },
+        jobName
+      );
 
       // Save data in DB
       for (const zone of zones) {
@@ -723,6 +415,28 @@ export const extractTrayAmount = (date, headless = true) => {
           `${dayjs(date).format("YYYY_MM_DD")}_extract_result.json`
         ),
         JSON.stringify(zones, null, 2)
+      );
+
+      console.log("Data saved successfully");
+      await updateJob(
+        {
+          actualState: "running",
+          lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+          lastLog: `Data saved successfully`,
+        },
+        jobName
+      );
+
+      sleep(2000);
+
+      console.log("Checking for missing days...");
+      await updateJob(
+        {
+          actualState: "running",
+          lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+          lastLog: `Checking for missing days...`,
+        },
+        jobName
       );
 
       // Check if all last days since the last extract have been extracted
@@ -746,7 +460,7 @@ export const extractTrayAmount = (date, headless = true) => {
           {
             actualState: "idle",
             lastRun: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-            lastLog: `Extraction completed successfully. Setting extraction for date ${dayToExtract.format(
+            lastLog: `Setting extraction for date ${dayToExtract.format(
               "YYYY-MM-DD"
             )}`,
             endAt: dayjs().format("YYYY-MM-DD HH:mm:ss"),
